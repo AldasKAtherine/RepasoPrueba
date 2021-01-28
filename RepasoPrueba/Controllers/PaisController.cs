8dd721e78c742cad60dc2e2bbb74f8a0a5180073
AldasKatherine <50203966+AldasKAtherine@users.noreply.github.com>
2021-01-28 00:02:31 -0500

    Servicio

---
+++ RepasoPrueba/Controllers/PaisController.cs
@@ -0,0 +1,49 @@
+﻿using RepasoPrueba.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace RepasoPrueba.Controllers
+{
+    public class PaisController : ApiController
+    {
+        private Datos data;
+        public PaisController()
+        {
+           
+            this.data = new Datos();
+        }
+
+        public IEnumerable<Pais> GetPaises()
+        {
+            return data.GetPaises();
+        }
+
+        // Ruta URL: "POST(Insert): api/categories"
+        public int PostPais([FromBody] Pais pais)
+        {
+            // Llamar a la Capa de Acceso a Datos
+            return data.InsertPais(pais);
+        }
+
+        // Ruta URL: "PUT(Update): api/categories/masks"
+        public int PutCategory(string id, [FromBody] Pais pais)
+        {
+            pais.IdPais = id;
+            // Llamar a la Capa de Acceso a Datos
+            return data.UpdatePais(pais);
+        }
+
+        public int DeleteCategory(string id)
+        {
+            Pais pais = new Pais();
+            pais.IdPais = id;
+            // Llamar a la Capa de Acceso a Datos
+            return data.DeletePais(pais);
+        }
+
+    }
+}
---
+++ RepasoPrueba/Models/Datos.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace RepasoPrueba.Models
+{
+    public class Datos
+    {
+        public Datos() { }
+        private string GetConnectionString()
+        {
+            return ConfigurationManager.ConnectionStrings["PaisesConnectionString"].ConnectionString;
+        }
+
+        public List<Pais> GetPaises()
+        {
+            List<Pais> lista = new List<Pais>();
+            string sql = @"SELECT [IdPais]
+                          ,[Nombre]
+                          FROM Pais";
+            using (SqlConnection connection = new SqlConnection(GetConnectionString()))
+            {
+                using (SqlCommand command = new SqlCommand(sql, connection))
+                {
+                    connection.Open();
+                    SqlDataReader reader = command.ExecuteReader(CommandBehavior.CloseConnection);
+                    while (reader.Read())
+                    {
+                        Pais ps = new Pais();
+                        ps.IdPais = reader["IdPais"].ToString();
+                        ps.Nombre = reader["Nombre"].ToString();
+                        lista.Add(ps);
+
+                    }
+                    reader.Close();
+                }
+            }
+            return lista;
+        }
+         public int InsertPais(Pais pais)
+         {
+            int afectadas;
+            string sql = @"INSERT INTO Pais ([IdPais] ,[Nombre])
+                            VALUES (@IdPais,@Nombre)";
+            using (SqlConnection connection = new SqlConnection(GetConnectionString()))
+            {
+                using (SqlCommand command = new SqlCommand(sql, connection))
+                {
+                    command.Parameters.AddWithValue("@IdPais", pais.IdPais);
+                    command.Parameters.AddWithValue("@ShortName", pais.Nombre);
+                    connection.Open();
+                    afectadas = command.ExecuteNonQuery();
+                    connection.Close();
+                }
+            }
+            return afectadas;
+        }
+
+        public int UpdatePais(Pais pais)
+        {
+            int afectadas;
+            string sql = @"UPDATE Pais 
+                           SET [Nombre] = @Nombre
+                         WHERE IdPais= @IdPais";
+            using (SqlConnection connection = new SqlConnection(GetConnectionString()))
+            {
+                using (SqlCommand command = new SqlCommand(sql, connection))
+                {
+                    command.Parameters.AddWithValue("@IdPais", pais.IdPais);
+                    command.Parameters.AddWithValue("@ShortName", pais.Nombre);
+                    connection.Open();
+                    afectadas = command.ExecuteNonQuery();
+                    connection.Close();
+                }
+            }
+            return afectadas;
+        }
+
+        public int DeletePais(Pais pais)
+        {
+            int afectadas;
+            string sql = @"DELETE FROM[dbo].[Pais]
+            WHERE IdPais=@Id";
+            using (SqlConnection connection = new SqlConnection(GetConnectionString()))
+            {
+                using (SqlCommand command = new SqlCommand(sql, connection))
+                {
+                    command.Parameters.AddWithValue("@Id", pais.IdPais);
+                    connection.Open();
+                    afectadas = command.ExecuteNonQuery();
+                    connection.Close();
+
+                }
+            }
+            return afectadas;
+        }
+    }
+}
